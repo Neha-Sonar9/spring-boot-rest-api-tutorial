079b9bc60fd1ace76420bd41e7dbceb92d891aa4
nsonar <sonaraneha9@gmail.com>
2025-12-16 16:33:14 +0530

    containerised and added helm charts

---
+++ dockerps.py
@@ -0,0 +1,15 @@
+def greet():
+    print("Hello")
+    return "Greetings!"
+   
+name= input("Enter your name: ")
+
+if name != " ":
+   print("Name is not empty.")
+else:
+    print("Name is empty.")
+
+
+    a=5
+    b=10
+    print("sum is equal to :", a+b) 
