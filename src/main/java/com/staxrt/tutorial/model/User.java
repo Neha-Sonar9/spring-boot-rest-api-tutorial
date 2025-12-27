a4e829a12f003e8441c18867c5e75cd39e666d41
nsonar <sonaraneha9@gmail.com>
2025-12-27 20:26:48 +0530

    Deploy Spring Boot app to Minikube and clean up Kubernetes manifests

--- src/main/java/com/staxrt/tutorial/model/User.java
+++ src/main/java/com/staxrt/tutorial/model/User.java
@@ -1,235 +1,68 @@
-/*
- *
- *  Copyright (c) 2018-2020 Givantha Kalansuriya, This source is a part of
- *   Staxrt - sample application source code.
- *   http://staxrt.com
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- *
- */
-
 package com.staxrt.tutorial.model;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
-/**
- * The type User.
- *
- * @author Givantha Kalansuriya
- */
 @Entity
 @Table(name = "users")
-@EntityListeners(AuditingEntityListener.class)
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Column(name = "first_name", nullable = false)
+    @NotBlank
+    @Column(name = "first_name")
     private String firstName;
 
-    @Column(name = "last_name", nullable = false)
+    @NotBlank
+    @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "email_address", nullable = false)
-    private String email;
+    @NotBlank
+    @Email
+    @Column(name = "email_address", unique = true)
+    private String emailAddress;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
 
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "created_at", nullable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private Date createdAt;
-
-    @Column(name = "created_by", nullable = true)
-    @CreatedBy
-    //private String createdBy;
 
     @UpdateTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "updated_at", nullable = false)
     private Date updatedAt;
 
-    @Column(name = "updated_by", nullable = true)
-    @LastModifiedBy
-    // private String updatedBy;
+    // Getters and Setters
+    public Long getId() { return id; }
 
-  /**
-   * Gets id.
-   *
-   * @return the id
-   */
-  public long getId() {
-        return id;
-    }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
 
-  /**
-   * Sets id.
-   *
-   * @param id the id
-   */
-  public void setId(long id) {
-        this.id = id;
-    }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
-  /**
-   * Gets first name.
-   *
-   * @return the first name
-   */
-  public String getFirstName() {
-        return firstName;
-    }
+    public String getEmailAddress() { return emailAddress; }
+    public void setEmailAddress(String emailAddress) { this.emailAddress = emailAddress; }
 
-  /**
-   * Sets first name.
-   *
-   * @param firstName the first name
-   */
-  public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    public String getCreatedBy() { return createdBy; }
+    public void setCreatedBy(String createdBy) { this.createdBy = createdBy; }
 
-  /**
-   * Gets last name.
-   *
-   * @return the last name
-   */
-  public String getLastName() {
-        return lastName;
-    }
+    public String getUpdatedBy() { return updatedBy; }
+    public void setUpdatedBy(String updatedBy) { this.updatedBy = updatedBy; }
 
-  /**
-   * Sets last name.
-   *
-   * @param lastName the last name
-   */
-  public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-  /**
-   * Gets email.
-   *
-   * @return the email
-   */
-  public String getEmail() {
-        return email;
-    }
-
-  /**
-   * Sets email.
-   *
-   * @param email the email
-   */
-  public void setEmail(String email) {
-        this.email = email;
-    }
-
-  /**
-   * Gets created at.
-   *
-   * @return the created at
-   */
-  public Date getCreatedAt() {
-        return createdAt;
-    }
-
-  /**
-   * Sets created at.
-   *
-   * @param createdAt the created at
-   */
-  public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-  /**
-   * Gets created by.
-   *
-   * @return the created by
-   */
-  public String getCreatedBy() {
-        return createdBy;
-    }
-
-  /**
-   * Sets created by.
-   *
-   * @param createdBy the created by
-   */
-  public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-  /**
-   * Gets updated at.
-   *
-   * @return the updated at
-   */
-  public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-  /**
-   * Sets updated at.
-   *
-   * @param updatedAt the updated at
-   */
-  public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-  /**
-   * Gets updated by.
-   *
-   * @return the updated by
-   */
-  public String getUpdatedBy() {
-        return updatedBy;
-    }
-
-  /**
-   * Sets updated by.
-   *
-   * @param updatedBy the updated by
-   */
-  public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", createdAt=" + createdAt +
-                ", createdBy='" + createdBy + '\'' +
-                ", updatedAt=" + updatedAt +
-                ", updatedby='" + updatedBy + '\'' +
-                '}';
-    }
-
-
+    public Date getCreatedAt() { return createdAt; }
+    public Date getUpdatedAt() { return updatedAt; }
 }
